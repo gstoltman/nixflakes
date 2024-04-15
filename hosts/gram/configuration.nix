165c63c6dc9ed1fb8f910d8fa0df59e9b519a806
gstoltman <gstoltman@gmail.com>
2024-04-15 16:58:55 -0700

    cleaning configuration.nix's

--- hosts/gram/configuration.nix
+++ hosts/gram/configuration.nix
@@ -15,6 +15,13 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nix.settings.auto-optimise-store = true;
+
+  # Garbage Collector
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 60d";
+  };
  
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -23,11 +30,6 @@
   boot.initrd.luks.devices."luks-7a6506cd-e9e6-4d21-bb2e-90a25c527df8".device = "/dev/disk/by-uuid/7a6506cd-e9e6-4d21-bb2e-90a25c527df8";
   
   networking.hostName = "gram"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -67,6 +69,7 @@
     #    package = pkgs.i3-gaps;
     #  };
     };
+    # Touchpad
     libinput = {
       enable = true;
       touchpad.naturalScrolling = true;
@@ -82,12 +85,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -153,31 +150,6 @@
     '';
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Release version on installation, no need to change.
 
 }
