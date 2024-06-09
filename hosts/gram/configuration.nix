1dfd37d53b16bb487c27402147afd863b093dc3e
gstoltman <gstoltman@gmail.com>
2024-06-09 11:20:13 -0700

    gram update and heat fix

--- hosts/gram/configuration.nix
+++ hosts/gram/configuration.nix
@@ -126,32 +126,15 @@
     PATH = [ "${XDG_BIN_HOME}" "${GOPATH}" ];
   };
 
-  # Fix for CPU Overheat on Gram
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelParams = ["intel_pstate=disable"];
   services.power-profiles-daemon.enable = false;
   services.tlp = {
     enable = true;
-    extraConfig = ''
-      START_CHARGE_THRESH_BAT0=75
-      STOP_CHARGE_THRESH_BAT0=95
-      CPU_SCALING_GOVERNOR_ON_AC=schedutil
-      CPU_SCALING_GOVERNOR_ON_BAT=schedutil
-      CPU_SCALING_MIN_FREQ_ON_AC=400000
-      CPU_SCALING_MAX_FREQ_ON_AC=2300000
-      CPU_SCALING_MIN_FREQ_ON_BAT=400000
-      CPU_SCALING_MAX_FREQ_ON_BAT=2300000
-      # Runtime Power Management for PCI(e) bus devices: on=disable, auto=enable.
-      # Default: on (AC), auto (BAT)
-      RUNTIME_PM_ON_AC=on
-      RUNTIME_PM_ON_BAT=auto
-      # Battery feature drivers: 0=disable, 1=enable
-      # Default: 1 (all)
-      NATACPI_ENABLE=1
-      TPACPI_ENABLE=1
-      TPSMAPI_ENABLE=1
-    '';
+    settings = {
+      TLP_DEFAULT_MODE = "BAT";
+      TLP_PERSISTENT_DEFAULT = 1;
+    };
   };
+
 
   system.stateVersion = "23.11"; # Release version on installation, no need to change.
 
